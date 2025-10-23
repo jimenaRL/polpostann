c931436e1db84dd840eb7d824f7926082f63c205
jimenarsss <11072909+jimenaRL@users.noreply.github.com>
2025-10-23 12:04:36 +0200

    Add files via upload
    Add validations scripts

---
+++ parse_and_join_results.py
@@ -0,0 +1,128 @@
+import os
+from glob import glob
+import  pandas as pd
+
+BASEPATH = "/home/jimena/work/dev/polpostann"
+VERSION = "v3"
+OUTPUTSFOLDER = os.path.join(BASEPATH, "outputs", VERSION)
+RESULTSFOLDER = os.path.join(BASEPATH, "results", VERSION)
+os.makedirs(RESULTSFOLDER, exist_ok=True)
+
+MODELS = {
+    "v3": [
+        "zephyr-7b-beta",
+        "Mistral-Small-24B-Instruct-2501",
+        # "Llama-3.3-70B-Instruct",
+        "Mistral-Large-Instruct-2411",
+    ],
+}
+
+ANNOTATIONS = {
+
+    "multiple": [
+        "voteintention/multiple/all",
+        # "support/multiple/all",
+        # "criticism/multiple/all"
+    ],
+
+    "binary" : [
+        # "criticism/binary/lepen",
+        # "criticism/binary/macron",
+        # "criticism/binary/melenchon",
+        # "support/binary/lepen",
+        # "support/binary/macron",
+        # "support/binary/melenchon",
+        # "voteintention/binary/lepen",
+        # "voteintention/binary/macron",
+        # "voteintention/binary/melenchon",
+    ]
+}
+
+CHOICES = {
+    "multiple": ["Macron", "Mélenchon", "Le Pen", "None"],
+    "binary" : ["YES", "NO"]
+}
+
+DEFAULTANSWER = {
+    "multiple": "None",
+    "binary" : "NO"
+}
+
+SETTINGS = ANNOTATIONS.keys()
+
+def parseAnwers(whole_answer, model, setting):
+
+    whole_answer = whole_answer \
+        .strip() \
+        .split('\n')[0] \
+        .replace("(", "") \
+        .replace(")", "") \
+        .replace(":", "") \
+        .replace("'", "") \
+        .replace('"', "") \
+        .replace(".", "") \
+        .replace("·", "") \
+        .replace(",", "")
+
+    if "gpt-oss" in model:
+        whole_answer = whole_answer.split("assistantfinal")[-1]
+
+    if setting == "binary":
+        whole_answer = whole_answer.upper()
+
+    annotation = DEFAULTANSWER[setting]
+    for choice in CHOICES[setting]:
+        if whole_answer[:len(choice)] == choice:
+            annotation = choice
+
+    return annotation
+
+def extract_data(model, annotation, setting, df=None, columns=[]):
+
+    assert model in MODELS[VERSION]
+    assert setting in SETTINGS
+
+    folders = glob(os.path.join(OUTPUTSFOLDER, f"*{model}*"))
+    assert len(folders) == 1
+    folder = folders[0]
+
+    # get answers
+    colname = model
+    path_guided = os.path.join(folder, "guided", annotation, "llm_answer_0.csv")
+    df_guided = pd.read_csv(path_guided) \
+        .fillna("None") \
+        .rename(columns={"answer": colname})
+
+    df_guided[colname] = df_guided[colname].apply(lambda a: parseAnwers(a, model, setting))
+
+    if df is not None:
+        df = df.merge(df_guided, on=['idx', 'tweet'])
+    else:
+        df = df_guided
+
+    columns.extend([model])
+
+    return df, columns
+
+for setting in SETTINGS:
+
+    for annotation in ANNOTATIONS[setting]:
+
+        df = None
+        columns = ["idx", "tweet"]
+
+        for model in MODELS[VERSION]:
+            df, columns = extract_data(model, annotation, setting, df, columns)
+
+        df = df.assign(guidedMayorityVote=df[df.columns[2:]].mode(axis=1)[0])
+        df = df.rename(columns={
+            'guidedMayorityVote':"mayorityVote",
+            })
+        columns.append("mayorityVote")
+
+        path = os.path.join(RESULTSFOLDER, f"{annotation.replace('/', '_')}.csv")
+
+        df[columns].to_csv(path, index=False)
+
+        print(f"Results for annotation {annotation} results saved at {path}")
+        os.system(f"xan shuffle {path} | xan slice -l 5 | xan v")
---
+++ validations.py
@@ -0,0 +1,188 @@
+import os
+import csv
+import json
+import numpy as np
+import pandas as pd
+from sklearn.metrics import \
+    precision_recall_fscore_support, \
+    accuracy_score
+
+BASEPATH = "/home/jimena/work/dev/polpostann"
+GTFILE = os.path.join(BASEPATH, "ground_truth_v2_400.csv")
+VERSION = "v3"
+RESULTSFOLDER = os.path.join(BASEPATH, "results", VERSION)
+METRICSFOLDER = os.path.join(BASEPATH, "metrics", VERSION)
+os.makedirs(METRICSFOLDER, exist_ok=True)
+
+MODELS = {
+    "v3": [
+        "zephyr-7b-beta",
+        "Mistral-Small-24B-Instruct-2501",
+        # "Llama-3.3-70B-Instruct",
+        "Mistral-Large-Instruct-2411",
+        "mayorityVote",
+    ],
+}
+
+NBPARAMS = {
+    "zephyr-7b-beta": 7,
+    "gpt-oss-20b": 20,
+    "Ministral-8B-Instruct-2410": 8,
+    "Mistral-Small-24B-Instruct-2501": 24,
+    "Mistral-Small-3.1-24B-Instruct-2503": 24,
+    "Magistral-Small-2506": 24,
+    "Mistral-Small-24B-Instruct-2501": 24,
+    "max_model_len_8000_Qwen3-30B-A3B-Instruct-2507": 30,
+    "gpt-oss-120b": 120,
+    "Llama-3.3-70B-Instruct": 70,
+    "max_model_len_7000_Llama-3.3-70B-Instruct": 70,
+    "DeepSeek-R1-Distill-Llama-70B": 70,
+    "Mistral-Large-Instruct-2411": 123,
+    "mayorityVote": -1,
+}
+
+SUPPORTCHOICES = {
+    "multiple": ["Macron", "Mélenchon", "Le Pen", "None"],
+    "binary" : ["YES"]
+}
+
+NBDECIMALS = 2
+
+ANNOTATIONS = [
+    "voteintention/multiple/all",
+    # "support/multiple/all",
+    # "criticism/multiple/all",
+    # "criticism/binary/lepen",
+    # "criticism/binary/macron",
+    # "criticism/binary/melenchon",
+    # "support/binary/lepen",
+    # "support/binary/macron",
+    # "support/binary/melenchon",
+    # "voteintention/binary/lepen",
+    # "voteintention/binary/macron",
+    # "voteintention/binary/melenchon"
+]
+
+
+for annotation in ANNOTATIONS:
+
+    file = os.path.join(RESULTSFOLDER, f"{annotation.replace('/', '_')}.csv")
+    candidate = annotation.split('/')[-1]
+    setting = annotation.split('/')[1]
+    task = annotation.split('/')[0]
+    column = f"{candidate.upper()} {task.upper()}"
+
+    with open(GTFILE, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        gt = [r[column] for r in reader]
+
+    with open(file, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        annotations = [r for r in reader]
+
+    idxs = range(len(gt))
+
+    metrics = []
+    for model in MODELS[VERSION]:
+
+        model_abb = model.split('000_')[-1]
+
+        ann = [a[model] for a in annotations]
+
+        # binary classification
+        if setting == "binary":
+            res = precision_recall_fscore_support(
+                y_true=gt,
+                y_pred=ann,
+                pos_label='YES',
+                average='binary',
+                zero_division=np.nan)
+
+            res = list(res)
+            support =  sum([1 if _ == 'YES' else 0 for _ in gt])
+
+            metrics.append({
+                "model": model_abb,
+                "kind": kind,
+                "params": NBPARAMS[model],
+                "precision": str(res[0])[:NBDECIMALS + 2],
+                "recall": str(res[1])[:NBDECIMALS + 2],
+                "f1_weighted": str(res[2])[:NBDECIMALS + 2],
+                "support": support,
+                "labels": ' | '.join(SUPPORTCHOICES[setting])
+                })
+
+        # multiclass classification
+        if setting == "multiple":
+            # See: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.precision_recall_fscore_support.html
+
+            acc = accuracy_score(
+                y_true=gt,
+                y_pred=ann,
+                normalize=True)
+
+            # binary
+            # Reports results for the classes specified by labels
+            res = precision_recall_fscore_support(
+                y_true=gt,
+                y_pred=ann,
+                labels=SUPPORTCHOICES['multiple'],
+                average=None,
+                zero_division=np.nan)
+
+            # 'macro'
+            # Calculate metrics for each label, and find their unweighted mean.
+            # This does not take label imbalance into account.
+            f1_macro = precision_recall_fscore_support(
+                y_true=gt,
+                y_pred=ann,
+                labels=SUPPORTCHOICES['multiple'],
+                average='macro',
+                zero_division=np.nan)[2]
+
+            # 'micro'
+            # Calculate metrics globally by counting the total true positives,
+            # false negatives and false positives
+            f1_micro = precision_recall_fscore_support(
+                y_true=gt,
+                y_pred=ann,
+                labels=SUPPORTCHOICES['multiple'],
+                average='micro',
+                zero_division=np.nan)[2]
+
+            # 'weighted'
+            # Calculate metrics for each label,
+            # and find their average weighted by support (the number of true instances for each label).
+            # This alters ‘macro’ to account for label imbalance;
+            # it can result in an F-score that is not between precision and recall.
+            f1_weighted = precision_recall_fscore_support(
+                y_true=gt,
+                y_pred=ann,
+                labels=SUPPORTCHOICES['multiple'],
+                average='weighted',
+                zero_division=np.nan)[2]
+
+            metrics.append({
+                "model": model_abb,
+                "params": NBPARAMS[model],
+                "support": ' | '.join(map(str, res[3])),
+                "labels": ' | '.join(SUPPORTCHOICES[setting]),
+                "accuracy": acc,
+                "precision": ' | '.join([str(r)[:NBDECIMALS + 2] for r in res[0]]),
+                "recall": ' | '.join([str(r)[:NBDECIMALS + 2] for r in res[1]]),
+                "f1_binary": ' | '.join([str(r)[:NBDECIMALS + 2] for r in res[2]]),
+                "f1_macro": str(f1_macro)[:NBDECIMALS + 2],
+                "f1_micro":  str(f1_micro)[:NBDECIMALS + 2],
+                "f1_weighted": str(f1_weighted)[:NBDECIMALS + 2],
+                })
+
+
+    df = pd.DataFrame.from_records(metrics).sort_values(by=["params"])
+    path = os.path.join(METRICSFOLDER, f"{annotation.replace('/', '_')}.csv")
+    df.to_csv(path, index=False)
+    print(f"Metrics for annotation {annotation} experiment saved at {path}")
+
+    cmd = f"xan select model,params,labels,support,accuracy,f1_macro,f1_binary {path} | xan sort -s f1_macro | xan v"
+    print(cmd)
+    os.system(cmd)
+
