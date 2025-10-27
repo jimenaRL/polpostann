ce451cb9fc194e786755f50d6ca872d76fde7983
jimenaRL <jimena@melix.net>
2025-10-27 15:53:36 +0100

    Ass nb gpus and partition as argument

--- voteintention_multiple_all_Mistral-Large-Instruct-2411.sh
+++ voteintention_multiple_all_Mistral-Large-Instruct-2411.sh
@@ -1,20 +1,25 @@
-export MODELPARAMS="'{\"model\": \"mistralai/Mistral-Large-Instruct-2411\", \"tokenizer_mode\": \"mistral\", \"config_format\": \"mistral\", \"load_format\": \"mistral\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 1, \"tensor_parallel_size\": 4}'"
+PARTITION=$1
+NBGPUS=$2
+
+export MODELPARAMS="'{\"model\": \"mistralai/Mistral-Large-Instruct-2411\", \"tokenizer_mode\": \"mistral\", \"config_format\": \"mistral\", \"load_format\": \"mistral\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 1, \"tensor_parallel_size\": ${NBGPUS}}'"
 export SAMPLINGPARAMS="'{\"temperature\": 0.15, \"seed\": 1, \"max_tokens\": 256}'"
 export NAME=Mistral-Large-Instruct-2411-seed1
 
 export TWEETSFILE=/lustre/fswork/projects/rech/nmf/umu89ib/dev/polpostann/enumerated_cleaned_text2annotate_2022-03-27_2022-04-25.csv
 export TWEETSCOLUMN=french
-export SYSTEMPROMT=/lustre/fswork/projects/rech/nmf/umu89ib/dev/polpostann/prompts/system/system_prompt_voteintention_multiple_all_french.txt
+export SYSTEMPROMT=/lustre/fswork/projects/rech/nmf/umu89ib/dev/polpostann/prompts/system/system_prompt_french.txt
 export USERPROMT=/lustre/fswork/projects/rech/nmf/umu89ib/dev/polpostann/prompts/user/user_prompt_voteintention_multiple_all_french.txt
 
 export BASEPATH=/lustre/fswork/projects/rech/nmf/umu89ib/dev/polpostann
 export OUTFOLDER=${BASEPATH}/outputs/french/${NAME}/guided/voteintention/multiple/all
 
+echo ${PARTITION}
+echo ${NBGPUS}
+
 sbatch \
     --job-name=${NAME} \
+    --ntasks-per-node=${NBGPUS} \
+    --gres=gpu:${PARTITION}:${NBGPUS} \
     --output=${OUTFOLDER}/%j.log  \
     --error=${OUTFOLDER}/%j.out  \
-    --ntasks-per-node=4 \
-    --gres=gpu:h100:4 \
-    --export=ALL \
-    multipleChoicesAllPrompts.slurm
+    multipleChoicesAllPrompt_${PARTITION}_${SERVER}.slurm
