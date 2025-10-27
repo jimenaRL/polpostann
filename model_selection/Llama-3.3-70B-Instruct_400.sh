eb2517b84c441a5a2d07932bf08071fbdbf3d54c
jimenaRL <jimena@melix.net>
2025-10-27 17:05:19 +0100

    Update commands

--- model_selection/Llama-3.3-70B-Instruct_400.sh
+++ model_selection/Llama-3.3-70B-Instruct_400.sh
@@ -1,6 +1,7 @@
 LANGUAGE=$1
 TASK=$2
 SEED=$3
+GPU=$4
 
 export MODELPARAMS="'{\"model\": \"meta-llama/Llama-3.3-70B-Instruct\", \"guided_decoding_backend\": \"xgrammar\", \"max_model_len\": 1200, \"seed\": ${SEED}, \"tensor_parallel_size\": 2}'"
 export SAMPLINGPARAMS="'{\"seed\": ${SEED}, \"max_tokens\": 256}'"
@@ -11,7 +12,7 @@ export TWEETSCOLUMN=${LANGUAGE}
 export SYSTEMPROMT=${POLPOSTANNPATH}/prompts/system/system_prompt_${LANGUAGE}.txt
 export USERPROMT=${POLPOSTANNPATH}/prompts/user/user_prompt_${TASK}_multiple_all_${LANGUAGE}.txt
 
-export OUTFOLDER=${POLPOSTANNPATH}/outputs/v3ModelSelection${LANGUAGE}/${NAME}/guided/${TASK}/multiple/all
+export OUTFOLDER=${POLPOSTANNPATH}/outputs_${SERVER}/v3ModelSelection${LANGUAGE}/${NAME}/guided/${TASK}/multiple/all
 
 sbatch \
     --job-name=${NAME} \
@@ -20,4 +21,4 @@ sbatch \
     --ntasks-per-node=2 \
     --gres=gpu:h100:2 \
     --export=ALL \
-    multipleChoicesAllPrompt_h100_jeanzay.slurm
+    ${SERVER}/multipleChoicesAllPrompt_${GPU}_${SERVER}.slurm
