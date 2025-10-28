a0b936ff158f860b96553346bf676567fbf11a2e
jimenaRL <jimena@melix.net>
2025-10-28 18:02:24 +0100

    Add more model selection binary scripts

---
+++ model_selection/binary/Llama-3.3-70B-Instruct_400.sh
@@ -0,0 +1,26 @@
+LANGUAGE=$1    # frech
+TASK=$2        # criticism
+SEED=$3        # 1
+GPU=$4         # h100
+CANDIDATE=$5   # macron
+
+export MODELPARAMS="'{\"model\": \"meta-llama/Llama-3.3-70B-Instruct\", \"guided_decoding_backend\": \"xgrammar\", \"max_model_len\": 1200, \"seed\": ${SEED}, \"tensor_parallel_size\": 2}'"
+export SAMPLINGPARAMS="'{\"seed\": ${SEED}, \"max_tokens\": 256}'"
+export NAME=Llama-3.3-70B-Instruct-seed${SEED}
+
+export TWEETSFILE=${POLPOSTANNPATH}/400_balanced_sampled_xan_seed_999_fr_en.csv
+export TWEETSCOLUMN=${LANGUAGE}
+export SYSTEMPROMT=${POLPOSTANNPATH}/prompts/system/system_prompt_${LANGUAGE}.txt
+export USERPROMT=${POLPOSTANNPATH}/prompts/user/user_prompt_${TASK}_binary_${CANDIDATE}_${LANGUAGE}.txt
+export CHOICES="OUI,NON"
+
+export OUTFOLDER=${POLPOSTANNPATH}/outputs_${SERVER}/v3ModelSelection${LANGUAGE}/${NAME}/guided/${TASK}/binary/${CANDIDATE}
+
+sbatch \
+    --job-name=${NAME} \
+    --output=${OUTFOLDER}/%j.log  \
+    --error=${OUTFOLDER}/%j.out  \
+    --ntasks-per-node=1 \
+    --gres=gpu:h100:1 \
+    --export=ALL \
+    ${SERVER}/annotate_tweets_${GPU}_${SERVER}.slurm
---
+++ model_selection/binary/Mistral-Large-Instruct-2411_400.sh
@@ -0,0 +1,26 @@
+LANGUAGE=$1    # frech
+TASK=$2        # criticism
+SEED=$3        # 1
+GPU=$4         # h100
+CANDIDATE=$5   # macron
+
+export MODELPARAMS="'{\"model\": \"mistralai/Mistral-Large-Instruct-2411\", \"tokenizer_mode\": \"mistral\", \"config_format\": \"mistral\", \"load_format\": \"mistral\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": ${SEED}, \"tensor_parallel_size\": 4}'"
+export SAMPLINGPARAMS="'{\"temperature\": 0.15, \"seed\": ${SEED}, \"max_tokens\": 256}'"
+export NAME=Mistral-Large-Instruct-2411-seed${SEED}
+
+export TWEETSFILE=${POLPOSTANNPATH}/400_balanced_sampled_xan_seed_999_fr_en.csv
+export TWEETSCOLUMN=${LANGUAGE}
+export SYSTEMPROMT=${POLPOSTANNPATH}/prompts/system/system_prompt_${LANGUAGE}.txt
+export USERPROMT=${POLPOSTANNPATH}/prompts/user/user_prompt_${TASK}_binary_${CANDIDATE}_${LANGUAGE}.txt
+export CHOICES="OUI,NON"
+
+export OUTFOLDER=${POLPOSTANNPATH}/outputs_${SERVER}/v3ModelSelection${LANGUAGE}/${NAME}/guided/${TASK}/binary/${CANDIDATE}
+
+sbatch \
+    --job-name=${NAME} \
+    --output=${OUTFOLDER}/%j.log  \
+    --error=${OUTFOLDER}/%j.out  \
+    --ntasks-per-node=1 \
+    --gres=gpu:h100:1 \
+    --export=ALL \
+    ${SERVER}/annotate_tweets_${GPU}_${SERVER}.slurm
