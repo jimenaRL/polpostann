247676e926ecda96894c739578a5dfdd1fe558af
jimenaRL <jimena@melix.net>
2025-10-28 18:06:55 +0100

    Add more model selection binary scripts

---
+++ model_selection/binary/voteintention_commands.sh
@@ -0,0 +1,15 @@
+./model_selection/binary/zephyr-7b-beta_400.sh french  1 h100 macron
+./model_selection/binary/zephyr-7b-beta_400.sh french voteintention 1 h100 lepen
+./model_selection/binary/zephyr-7b-beta_400.sh french voteintention 1 h100 melenchon
+
+./model_selection/binary/Mistral-Small-24B-Instruct-2501_400.sh french voteintention 1 h100 macron
+./model_selection/binary/Mistral-Small-24B-Instruct-2501_400.sh french voteintention 1 h100 lepen
+./model_selection/binary/Mistral-Small-24B-Instruct-2501_400.sh french voteintention 1 h100 melenchon
+
+./model_selection/binary/Llama-3.3-70B-Instruct_400.sh french voteintention 1 h100 macron
+./model_selection/binary/Llama-3.3-70B-Instruct_400.sh french voteintention 1 h100 lepen
+./model_selection/binary/Llama-3.3-70B-Instruct_400.sh french voteintention 1 h100 melenchon
+
+./model_selection/binary/Mistral-Large-Instruct-2411_400.sh french voteintention 1 h100 macron
+./model_selection/binary/Mistral-Large-Instruct-2411_400.sh french voteintention 1 h100 lepen
+./model_selection/binary/Mistral-Large-Instruct-2411_400.sh french voteintention 1 h100 melenchon
