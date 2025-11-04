f2ac861757e81d1822d2f77ac7c7429b4f493e1a
jimenaRL <jimena@melix.net>
2025-11-04 09:11:29 +0100

    Update binary tasks

--- commands_task_binary_candidate.sh
+++ commands_task_binary_candidate.sh
@@ -1,25 +1,24 @@
-./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh voteintention 1 a100 macron
-./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh voteintention 1 a100 lepen
-./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh voteintention 1 a100 melenchon
+./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh voteintention 1 h100 macron
+./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh voteintention 1 h100 lepen
+./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh voteintention 1 h100 melenchon
 
-./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh criticism 1 a100 macron
-./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh criticism 1 a100 lepen
-./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh criticism 1 a100 melenchon
+./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh criticism 1 h100 macron
+./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh criticism 1 h100 lepen
+./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh criticism 1 h100 melenchon
 
-./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh support 1 a100 macron
-./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh support 1 a100 lepen
-./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh support 1 a100 melenchon
+./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh support 1 h100 macron
+./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh support 1 h100 lepen
+./task_binary_candidate_french_Mistral-Small-24B-Instruct-2501.sh support 1 h100 melenchon
 
 
+./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh voteintention 1 h100 macron
+./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh voteintention 1 h100 lepen
+./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh voteintention 1 h100 melenchon
 
-./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh voteintention 1 a100 macron
-./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh voteintention 1 a100 lepen
-./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh voteintention 1 a100 melenchon
+./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh criticism 1 h100 macron
+./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh criticism 1 h100 lepen
+./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh criticism 1 h100 melenchon
 
-./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh criticism 1 a100 macron
-./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh criticism 1 a100 lepen
-./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh criticism 1 a100 melenchon
-
-./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh support 1 a100 macron
-./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh support 1 a100 lepen
-./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh support 1 a100 melenchon
+./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh support 1 h100 macron
+./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh support 1 h100 lepen
+./task_binary_candidate_french_Mistral-Large-Instruct-2411.sh support 1 h100 melenchon
